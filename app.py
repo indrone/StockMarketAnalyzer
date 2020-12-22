fa9054a800fbd2b35f0fa01aff82637f37ebf916
indrone <indresh.bhattacharya@gmail.com>
2020-12-22 12:21:34 +0530

    flask added

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from scripts import fetch
 from scripts import viz
+from flask import Flask,render_template
 
-hist_data=fetch.historical("PNB.NS")
-print(hist_data)
-viz.plot_stock(hist_data["Close"])
+app=Flask(__name__)
+
+
+@app.route("/addPortfolio/",methods=["GET","POST"])
+def add_portfolio():
+
+    return render_template("header.html")
+
+
+#hist_data=fetch.historical("PNB.NS")
+#viz.plot_stock(hist_data["Close"])
+
+app.run()
